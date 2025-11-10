317021d9d5867d02407e1127c681d0289a708651
Pancho Labrador <plabradorjr@gmail.com>
2025-11-10 16:12:22 -0600

    a

--- strategy_config.py
+++ strategy_config.py
@@ -6,17 +6,17 @@ from hyperliquid_client import my_print  # type: ignore
 
 # Trading parameters
 params = {
-    "symbol": "MELANIA/USDC:USDC",
-    "timeframe": "15m",
+    "symbol": "MELANI/USDC:USDC",
+    "timeframe": "5m",
     "position_size_pct": 140.0,
     "leverage": 3,
     "margin_mode": "isolated",  # "isolated" or "cross"
     # Take-Profit and Stop-Loss settings
-    "tp_pct": 7,
+    "tp_pct": 9,
     "tp_size_pct": 60,
-    "sl_pct": 7,
+    "sl_pct": 8,
     # Trailing Stop-Loss settings (set to 0 to disable)
-    "trailing_sl_pct": 7,
+    "trailing_sl_pct": 8,
     # EMA settings
     "ema_fast": 30,
     "ema_slow": 40,
@@ -26,8 +26,8 @@ params = {
 }
 
 # Trading conditions to ignore
-ignore_longs = False
-ignore_shorts = True
+ignore_longs = True
+ignore_shorts = False
 ignore_exit = False
 ignore_tp = False
 ignore_sl = False
